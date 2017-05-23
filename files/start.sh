5a272fafacbcd25f458d922b46e1bb5b7ae8fb37
Khairul Fahmi <kyu.helf@gmail.com>
2017-05-23 14:29:44 +0700

    Fetch both engine and model; fix path

--- files/start.sh
+++ files/start.sh
@@ -3,20 +3,25 @@
 function copyArtifact() {
     echo "Getting build result from Github..."
 
-    ENGINE_DIR=/root/engines/recommender-incubating
+    ENGINE_ROOT=/root/engines
+    ENGINE_DIR=${ENGINE_ROOT}/${PIO_ENVIRONMENT}/recommender-incubating
     mkdir -p $ENGINE_DIR
 
     TEMP_DIR=/tmp/recommender
     mkdir -p $TEMP_DIR
     cd $TEMP_DIR
 
+    curl -o Engines.zip -L https://github.com/happyfresh/Engines/archive/${PIO_ENVIRONMENT}.zip
+    unzip Engines.zip
 
-    curl -LO https://github.com/kahirul/PIO-Model/archive/${PIO_ENVIRONMENT}.zip
-    unzip ${PIO_ENVIRONMENT}.zip
-    rm ${PIO_ENVIRONMENT}.zip
+    curl -o Model.zip -L https://github.com/kahirul/PIO-Model/archive/${PIO_ENVIRONMENT}.zip
+    unzip Model.zip
 
+    cp -r Engines-${PIO_ENVIRONMENT}/recommender-incubating/* $ENGINE_DIR/
     cp -r PIO-Model-${PIO_ENVIRONMENT}/* $ENGINE_DIR/
-    rm -fr PIO-Model-${PIO_ENVIRONMENT}
+    rm -fr PIO-Model-${PIO_ENVIRONMENT} Engines-${PIO_ENVIRONMENT} Engines.zip Model.zip
+
+    ln -s $ENGINE_DIR $ENGINE_ROOT/recommender-incubating
 
     echo "Done"
 }
