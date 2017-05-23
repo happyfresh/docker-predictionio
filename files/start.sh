bd0f9a4ee05a073a9b23199aa0d55c42aec81abf
Khairul Fahmi <kyu.helf@gmail.com>
2017-05-23 12:09:33 +0700

    Fix target directory

--- files/start.sh
+++ files/start.sh
@@ -3,15 +3,19 @@
 function copyArtifact() {
     echo "Getting build result from Github..."
 
+    ENGINE_DIR=/root/engines/recommender-incubating
+    mkdir -p $ENGINE_DIR
+
     TEMP_DIR=/tmp/recommender
     mkdir -p $TEMP_DIR
     cd $TEMP_DIR
+
 
     curl -LO https://github.com/kahirul/PIO-Model/archive/${PIO_ENVIRONMENT}.zip
     unzip ${PIO_ENVIRONMENT}.zip
     rm ${PIO_ENVIRONMENT}.zip
 
-    cp -r PIO-Model-${PIO_ENVIRONMENT}/* /root/engines/recommender-incubating/
+    cp -r PIO-Model-${PIO_ENVIRONMENT}/* $ENGINE_DIR/
     rm -fr PIO-Model-${PIO_ENVIRONMENT}
 
     echo "Done"
